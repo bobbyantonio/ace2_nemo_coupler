07e89900a27b43bb7c269716eddaab1cba32b357
Bobby Antonio <bobby.antonio@physics.ox.ac.uk>
2026-09-01 16:12:23 +0000

    Added samudrace analysis notebook

---
+++ notebooks/process_samudrace.py
@@ -0,0 +1,123 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: ece4
+#     language: python
+#     name: ece4
+# ---
+
+# %%
+import os, sys
+import datetime
+from glob import glob
+import pickle
+import pandas as pd
+import xarray as xr
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
+sys.path.append("/home/ecme4254/perm/repos/ace2_nemo_coupler")
+from notebooks.coupling_processing_utils import calculate_linear_relationship, calculate_anomalies, ace2_var_lookup, is_notebook, bjerknes_feedback_analysis
+
+# %%
+experiment_id='samudrace'
+if is_notebook():
+    years=range(1951,1955)
+    raw_data_dir = "/home/ecme4254/hpcperm/model_runs/samudrace/"
+    debug=True
+    base_output_dir ='/home/ecme4254/perm/repos/ace2_nemo_coupler/notebooks/processed_data'
+    ace2_data_dir = '/home/ecme4254/hpcperm/ml_model_data/ace2/'
+else:
+    parser = ArgumentParser()
+    parser.add_argument('--raw-data-dir', type=str, required=True)
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--years', type=str, default='1951-2021')
+    parser.add_argument('--ace2-data-dir', type=str, required=True)
+    parser.add_argument('--base-output-dir', type=str, required=True)
+    args = parser.parse_args()
+
+    debug = args.debug
+    raw_data_dir = args.raw_data_dir
+    experiment_id = args.experiment_id
+    ace2_data_dir = args.ace2_data_dir
+    years_split = args.years.split('-')
+    base_output_dir = args.base_output_dir
+    years = range(int(years_split[0]), int(years_split[1])+1)
+
+    if debug:
+        years = years[:10]
+
+
+OUTPUT_DIR = os.path.join(base_output_dir, experiment_id)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+# %%
+# ACE2 grid / sea mask
+ace2grid = xr.load_dataset(os.path.join(ace2_data_dir, "grid.nc"))
+sea_mask = xr.load_dataarray(os.path.join(ace2_data_dir, "era5_sea_mask_ACE2.nc"))
+ace2_grid_area = xr.load_dataset(os.path.join(ace2_data_dir, "gridarea.nc"))['cell_area']
+
+# %%
+atmosphere_ds = xr.load_dataset(samudrace_atmosphere_monthly_mean_predictions.nc")
+
+
+# %%
+atmosphere_ds = atmosphere_ds.rename({'PRATEsfc': 'total_precipitation', 
+                                'LHTFLsfc': 'mean_surface_latent_heat_flux', 
+                                'UGRD10m': '10m_u_component_of_wind',
+                                'TMP2m': '2m_temperature',
+                                'PRESsfc': 'surface_pressure',
+                                'lat': 'latitude', 
+                                'lon': 'longitude'})
+
+# %%
+atmosphere_ds['total_precipitation_daily'] = atmosphere_ds['total_precipitation']*86400
+atmosphere_ds = atmosphere_ds.isel(sample=0).drop_vars(['counts', 'time'])
+
+
+# %%
+ocean_ds = xr.load_dataset(os.path.join(raw_data_dir, "samudrace_ocean_monthly_mean_predictions.nc"))
+ocean_ds = ocean_ds.isel(sample=0).drop_vars(['counts'])
+
+# %%
+atmosphere_ds = atmosphere_ds.assign_coords(time=ocean_ds['valid_time'])
+
+
+# %%
+ocean_ds = ocean_ds.assign_coords(time=ocean_ds['valid_time'])
+ocean_ds = ocean_ds.rename({'sst': 'sea_surface_temperature',
+                            'ocean_sea_ice_fraction': 'sea_ice_fraction',
+                            'zos': 'sea_surface_height',
+                            'lat': 'latitude', 
+                            'lon': 'longitude'})
+
+# %%
+experiment_ds = xr.merge([atmosphere_ds, ocean_ds], compat='no_conflicts')
+
+# %%
+adjusted_time = [datetime.datetime(v.year - 311 + 1951, v.month, 15) for v in experiment_ds['time'].values]
+
+# %%
+experiment_ds = experiment_ds.assign_coords(time=adjusted_time)
+
+# %% [markdown]
+# ## Bjerknes feedback
+
+# %%
+results_dict, anomaly_ds = bjerknes_feedback_analysis(experiment_ds)
+
+# %%
+if not debug:
+    anomaly_ds[[v for v in anomaly_ds  if (v.endswith('gradient') or v.endswith('area_avg'))]].to_netcdf(os.path.join(OUTPUT_DIR, f'zonal_pacific_gradients.nc'))
+
+if not debug:
+    with open(os.path.join(OUTPUT_DIR, f'bjerknes_correlations.pkl'), 'wb+') as ofh:
+        pickle.dump(results_dict, ofh)
+
+# %%
