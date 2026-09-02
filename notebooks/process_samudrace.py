8a119685d123ac2f636da7c254e45751bbbdedb0
Bobby Antonio <bobby.antonio@physics.ox.ac.uk>
2026-09-02 16:53:59 +0100

    Refined notebook for processing samudrace

--- notebooks/process_samudrace.py
+++ notebooks/process_samudrace.py
@@ -18,6 +18,7 @@ import datetime
 from glob import glob
 import pickle
 import xarray as xr
+import xarray_regrid
 from pathlib import Path
 from argparse import ArgumentParser
 
@@ -27,7 +28,7 @@ if not str(base_dir).endswith('ace2_nemo_coupler'):
     base_dir = base_dir.parent
     
 sys.path.append(str(base_dir))
-from notebooks.coupling_processing_utils import calculate_linear_relationship, calculate_anomalies, ace2_var_lookup, is_notebook, bjerknes_feedback_analysis
+from notebooks.coupling_processing_utils import calculate_linear_relationship, calculate_anomalies, ace2_var_lookup, is_notebook, bjerknes_feedback_analysis, calculate_lagged_correlations
 
 # %%
 experiment_id='samudrace'
@@ -44,6 +45,7 @@ else:
     parser.add_argument('--years', type=str, default='1951-2021')
     parser.add_argument('--ace2-data-dir', type=str, required=True)
     parser.add_argument('--base-output-dir', type=str, required=True)
+    
     args = parser.parse_args()
 
     debug = args.debug
@@ -68,21 +70,29 @@ sea_mask = xr.load_dataarray(os.path.join(ace2_data_dir, "era5_sea_mask_ACE2.nc"
 ace2_grid_area = xr.load_dataset(os.path.join(ace2_data_dir, "gridarea.nc"))['cell_area']
 
 # %%
-atmosphere_ds = xr.load_dataset(os.path.join(raw_data_dir, "atmosphere", "monthly_mean_predictions.nc"))[['PRATEsfc', 'LHTFLsfc','UGRD10m','TMP2m','PRESsfc']]
+atmosphere_ds = xr.load_dataset(os.path.join(raw_data_dir, "atmosphere", "monthly_mean_predictions.nc"))[['PRATEsfc', 'LHTFLsfc', 'SHTFLsfc', 'UGRD10m','TMP2m','PRESsfc', 'DSWRFsfc']]
 
 
 # %%
 atmosphere_ds = atmosphere_ds.rename({'PRATEsfc': 'total_precipitation', 
-                                    'LHTFLsfc': 'mean_surface_latent_heat_flux', 
-                                    'UGRD10m': '10m_u_component_of_wind',
-                                    'TMP2m': '2m_temperature',
-                                    'PRESsfc': 'surface_pressure',
-                                    'lat': 'latitude', 
-                                    'lon': 'longitude'})
+                                'LHTFLsfc': 'mean_surface_latent_heat_flux', 
+                                'SHTFLsfc': 'mean_surface_sensible_heat_flux',
+                                'DSWRFsfc': 'mean_surface_downward_short_wave_radiation_flux',
+                                'UGRD10m': '10m_u_component_of_wind',
+                                'TMP2m': '2m_temperature',
+                                'PRESsfc': 'surface_pressure',
+                                'lat': 'latitude', 
+                                'lon': 'longitude'})
 
 # %%
 atmosphere_ds['total_precipitation_daily'] = atmosphere_ds['total_precipitation']*86400
 atmosphere_ds = atmosphere_ds.isel(sample=0).drop_vars(['counts', 'time'])
+
+# ACE2 sign convention for these fluxes is opposite to ECMWF convention of positive downward 
+atmosphere_ds['mean_surface_latent_heat_flux'] = -1 * atmosphere_ds['mean_surface_latent_heat_flux']
+atmosphere_ds['mean_surface_sensible_heat_flux'] = -1 * atmosphere_ds['mean_surface_sensible_heat_flux']
+
+atmosphere_ds['mean_surface_heat_flux'] = atmosphere_ds['mean_surface_latent_heat_flux'] + atmosphere_ds['mean_surface_sensible_heat_flux']
 
 
 # %%
@@ -96,8 +106,6 @@ atmosphere_ds = atmosphere_ds.assign_coords(time=ocean_ds['valid_time'])
 # %%
 ocean_ds = ocean_ds.assign_coords(time=ocean_ds['valid_time'])
 ocean_ds = ocean_ds.rename({'sst': 'sea_surface_temperature',
-                            'ocean_sea_ice_fraction': 'sea_ice_fraction',
-                            'zos': 'sea_surface_height',
                             'lat': 'latitude', 
                             'lon': 'longitude'})
 
@@ -110,6 +118,12 @@ adjusted_time = [datetime.datetime(v.year - 311 + 1951, v.month, 15) for v in ex
 # %%
 experiment_ds = experiment_ds.assign_coords(time=adjusted_time)
 
+# %%
+experiment_ds = experiment_ds.regrid.linear(sea_mask)
+
+# %%
+experiment_ds
+
 # %% [markdown]
 # ## Bjerknes feedback
 
@@ -117,9 +131,30 @@ experiment_ds = experiment_ds.assign_coords(time=adjusted_time)
 results_dict, anomaly_ds = bjerknes_feedback_analysis(experiment_ds)
 
 # %%
-if not debug:
-    anomaly_ds[[v for v in anomaly_ds  if (v.endswith('gradient') or v.endswith('area_avg'))]].to_netcdf(os.path.join(OUTPUT_DIR, f'zonal_pacific_gradients.nc'))
+anomaly_ds[[v for v in anomaly_ds  if (v.endswith('gradient') or v.endswith('area_avg'))]].to_netcdf(os.path.join(OUTPUT_DIR, f'zonal_pacific_gradients.nc'))
 
-if not debug:
-    with open(os.path.join(OUTPUT_DIR, f'bjerknes_correlations.pkl'), 'wb+') as ofh:
+with open(os.path.join(OUTPUT_DIR, f'bjerknes_correlations.pkl'), 'wb+') as ofh:
         pickle.dump(results_dict, ofh)
+
+# %% [markdown]
+# ## Lagged correlations
+
+# %%
+month_lag_max=0
+for lag_vars in [['mean_surface_heat_flux', 'sea_surface_temperature'],
+                    ['mean_surface_downward_short_wave_radiation_flux','sea_surface_temperature']
+                ]:
+    
+    print('Calculating lagged correlations for ', lag_vars, flush=True)
+    lag_var1 = lag_vars[0]
+    lag_var2 = lag_vars[1]
+    
+    ace2_nemo_results_dict = calculate_lagged_correlations(
+                                                            experiment_ds, 
+                                                            lag_var1, 
+                                                            lag_var2, 
+                                                            month_lag_max=month_lag_max)
+        
+    
+    with open(os.path.join(OUTPUT_DIR, f'lagged_correlations_max{month_lag_max}_{lag_var1}_{lag_var2}.pkl'), 'wb+') as ofh:
+        pickle.dump(ace2_nemo_results_dict, ofh)
