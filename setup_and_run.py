6df35246e2d4be16d1b9b291087029443de68452
Bobby Antonio <bobby.antonio@physics.ox.ac.uk>
2026-09-04 15:49:59 +0000

    Changes to include fwb in config

--- setup_and_run.py
+++ setup_and_run.py
@@ -114,8 +114,11 @@ if __name__ == "__main__":
     first_step_polling_timeout = config.get('first_step_polling_timeout', 12*3600)  # Timeout in seconds for polling ocean model output files on first step
     from_restart = config['from_restart']
     freshwater_balance_ind = config.get('freshwater_balance', 1)  # Default to 1 (on) if not specified
+<<<<<<< HEAD
     coastal_ice_flux_masking = config.get('coastal_ice_flux_masking', True)  # Default to True if not specified
     infer_solid_precip = config.get('infer_solid_precip', True)
+=======
+>>>>>>> 73b727e (Changes to include fwb in config)
     
     # Directory containing ecearth scripts to compile and setup run directory
     ece_script_dir=os.path.join(args.ecearth_dir, 'scripts')
@@ -205,8 +208,11 @@ if __name__ == "__main__":
     model_config:
         nemo:
             freshwater_balance: {freshwater_balance_ind}
+<<<<<<< HEAD
         coastal_ice_flux_masking: {coastal_ice_flux_masking}
         infer_solid_precip: {infer_solid_precip}
+=======
+>>>>>>> 73b727e (Changes to include fwb in config)
     """ 
         logger.info(yaml_content)
         
